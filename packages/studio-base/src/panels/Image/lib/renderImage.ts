27ea858d52a1174d1b9edfb52380d52be377b4ee
Eric Olson <eolson@oshkoshcorp.com>
2022-06-08 12:51:36 -0500

    Adjusted tabs vs. Spaces

--- packages/studio-base/src/panels/Image/lib/renderImage.ts
+++ packages/studio-base/src/panels/Image/lib/renderImage.ts
@@ -139,10 +139,10 @@ function decodeMessageToBitmap(
           break;
         case "rgb8":
           decodeRGB8(rawData, width, height, image.data);
-	  break;
-	case "rgba8":
-	  decodeRGBA8(rawData, width, height, image.data);
-	  break;
+          break;
+        case "rgba8":
+          decodeRGBA8(rawData, width, height, image.data);
+          break;
         case "bgr8":
         case "8UC3":
           decodeBGR8(rawData, width, height, image.data);
